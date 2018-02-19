ad386263bc4ef76caf66c8fac24d3caa0917d5b8
Garrett Brown <themagnificentmrb@gmail.com>
2018-02-19 11:19:17 -0800

    wip

--- xbmc/cores/RetroPlayer/environment/Environment.h
+++ xbmc/cores/RetroPlayer/environment/Environment.h
@@ -19,9 +19,6 @@
  */
 #pragma once
 
-#include "cores/RetroPlayer/playback/IFramePlayback.h"
-#include "cores/RetroPlayer/RetroPlayerClock.h"
-
 #include <memory>
 #include <stdint.h>
 
@@ -34,45 +31,43 @@ namespace GAME
 
 namespace RETRO
 {
-  class CAction;
-  class CGoal;
-  class CReward;
+  /*!
+   * \brief Video, audio, memory and haptics for a timeframe
+   */
   class CState;
 
-  class CEnvironment : public IFramePlayback
+  /*!
+   * \brief Combined state of all controllers and input for a timeframe
+   */
+  class CAction;
+
+  /*!
+   * \brief Directed goal set by an observer to allow NLP control of an agent
+   */
+  class CGoal;
+
+  /*!
+   * \brief Scores and achievements
+   */
+  class CReward;
+
+
+  /*!
+   * \brief class Environment transitions
+   */
+  class CEnvironment
   {
   public:
-    CEnvironment(GAME::CGameClient &gameClient);
-
-    ~CEnvironment() override;
-
-    // Implementation of IFramePlayback
-    uint64_t PastFramesAvailable() const override;
-    uint64_t FutureFramesAvailable() const override;
-    bool HasCurrentFrame() const override;
-    uint64_t MaxFrameCount() const override;
-    void SeekFrame(int64_t offsetFrames) override;
-    void SetMaxFrameCount(uint64_t frameCount) override;
-    void FrameEvent() override;
-
-  private:
-    // Environment functions
-    static void UpdateState( uint64_t t,       CState& state, const CReward& reward, const CGoal& goal, const CAction& action);
-    static void UpdateReward(uint64_t t, const CState& state,       CReward& reward, const CGoal& goal, const CAction& action);
-    static void UpdateGoal(  uint64_t t, const CState& state, const CReward& reward,       CGoal& goal, const CAction& action);
-    static void UpdateAction(uint64_t t, const CState& state, const CReward& reward, const CGoal& goal,       CAction& action);
-
-    // Construction parameters
-    GAME::CGameClient &m_gameClient;
+    static void UpdateState(        CState& state, const CReward& reward, const CGoal& goal, const CAction& action );
+    static void UpdateReward( const CState& state,       CReward& reward, const CGoal& goal, const CAction& action );
+    static void UpdateGoal(   const CState& state, const CReward& reward,       CGoal& goal, const CAction& action );
+    static void UpdateAction( const CState& state, const CReward& reward, const CGoal& goal,       CAction& action );
 
     // Environment variables
     std::unique_ptr<CState> m_state;
     std::unique_ptr<CReward> m_reward;
     std::unique_ptr<CGoal> m_goal;
     std::unique_ptr<CAction> m_action;
-
-    bool m_bStop = false;
-    uint64_t m_timestamp = 0;
   };
 }
 }
