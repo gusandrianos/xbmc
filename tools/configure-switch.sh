accbd194241525cc7fb9a4cf1e2c4cd3b1661461
VelocityRa <makren67@gmail.com>
2018-10-09 00:28:20 +0300

    [WIP] Nintendo Switch port

---
+++ tools/configure-switch.sh
@@ -0,0 +1,10 @@
+source /etc/profile.d/devkit-env.sh
+
+./configure \
+    --disable-debug \
+    --with-platform=switch \
+    --host=aarch64-none-elf \
+    --with-toolchain="${DEVKITPRO}/devkitA64" \
+    --prefix="${HOME}/switch-tools/kodi-deps" \
+    --with-tarballs="${HOME}/switch-tools/kodi-tarballs"
+
