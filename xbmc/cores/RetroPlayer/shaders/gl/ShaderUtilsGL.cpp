b56009bc8014c6bd7deee241fa14de56dbee70f3
Kostas Andrianos <gusandrianos@gmail.com>
2019-06-14 14:21:06 +0300

    Add new class ShaderUtilsGL

---
+++ xbmc/cores/RetroPlayer/shaders/gl/ShaderUtilsGL.cpp
@@ -0,0 +1,33 @@
+/*
+ *  Copyright (C) 2019 Team Kodi
+ *  This file is part of Kodi - https://kodi.tv
+ *
+ *  SPDX-License-Identifier: GPL-2.0-or-later
+ *  See LICENSES/README.md for more information.
+ */
+
+#include "ShaderUtilsGL.h"
+
+using namespace KODI;
+using namespace SHADER;
+
+GLint CShaderUtilsGL::TranslateWrapType(WRAP_TYPE wrap)
+{
+    GLint glWrap;
+    switch(wrap)
+    {
+        case WRAP_TYPE_EDGE:
+            glWrap = GL_CLAMP_TO_EDGE;
+            break;
+        case WRAP_TYPE_REPEAT:
+            glWrap = GL_REPEAT;
+            break;
+        case WRAP_TYPE_MIRRORED_REPEAT:
+            glWrap = GL_MIRRORED_REPEAT;
+            break;
+        case WRAP_TYPE_BORDER:
+        default:
+            glWrap = GL_CONSTANT_BORDER;
+    }
+    return glWrap;
+}
---
+++ xbmc/cores/RetroPlayer/shaders/gl/ShaderUtilsGL.h
@@ -0,0 +1,25 @@
+/*
+ *  Copyright (C) 2019 Team Kodi
+ *  This file is part of Kodi - https://kodi.tv
+ *
+ *  SPDX-License-Identifier: GPL-2.0-or-later
+ *  See LICENSES/README.md for more information.
+ */
+
+#pragma once
+
+#include "cores/RetroPlayer/shaders/ShaderTypes.h"
+
+#include <GL/gl.h>
+
+namespace KODI
+{
+    namespace SHADER
+    {
+        class CShaderUtilsGL
+        {
+        public:
+            static GLint TranslateWrapType(WRAP_TYPE wrap);
+        };
+    }
+}
